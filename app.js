98381578b1e9f7d16810a977422585a3447f06cb
Djisu <pfleischer2002@yahoo.co.uk>
2021-04-26 23:10:45 +0000

    Commit app.js to bring in javascript functionality

---
+++ app.js
@@ -0,0 +1,9 @@
+const reasonInput = document.querySelector('#input-reason')
+const reviewInput = document.querySelector('#input-review')
+const cancelBtn = document.querySelector('#btn-cancel')
+const confirmBtn = documeny.querySelector('#btn-confirm')
+
+//Add event listeners to the buttons
+confirmBtn.addEventLIstener('click', () => {
+    
+})
