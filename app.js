775cc48e0ec969bbaef147a4c5ba332c0c59b01a
Djisu <pfleischer2002@yahoo.co.uk>
2021-04-27 00:36:42 +0000

    commit bottons and total values

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const reasonInput = document.querySelector('#input-reason')
 const reviewInput = document.querySelector('#input-review')
 const cancelBtn = document.querySelector('#btn-cancel')
-const confirmBtn = documeny.querySelector('#btn-confirm')
+const confirmBtn = document.querySelector('#btn-confirm')
+const reviewsList = document.querySelector('#reviews-list')
+const totalReviewsOutput = document.querySelector('#total-reviews')
+
+let totalReviews = 0
+
+const clear = () => {
+  reasonInput.value = ''
+  reviewInput.value = ''
+}
 
 //Add event listeners to the buttons
-confirmBtn.addEventLIstener('click', () => {
-    
+confirmBtn.addEventListener('click', () => {
+  const enteredReason = reasonInput.value
+  const enteredReview = reviewInput.value
+
+  if (
+    enteredReason.trim().length <= 0 ||
+    enteredReview <= 0 ||
+    enteredReview.trim().length <= 0
+  ) {
+    return
+  }
+
+  const newItem = document.createElement('ion-item')
+  newItem.textContent = enteredReason + ': $' + enteredReview
+
+  reviewsList.appendChild(newItem)
+
+  totalReviews += enteredReview
+  clear()
 })
+
+cancelBtn.addEventListener('click', clear)
